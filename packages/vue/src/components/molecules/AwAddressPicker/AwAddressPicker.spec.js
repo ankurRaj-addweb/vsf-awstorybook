70e3384adb7f61fce78d2e89a4b98b4c97148073
ankurRaj-addweb <ankur.r@addwebsolution.in>
2022-04-04 18:10:06 +0530

    JSbugs

--- packages/vue/src/components/molecules/AwAddressPicker/AwAddressPicker.spec.js
+++ packages/vue/src/components/molecules/AwAddressPicker/AwAddressPicker.spec.js
@@ -1,7 +1,7 @@
 import { shallowMount } from "@vue/test-utils";
 import AwAddressPicker from "./AwAddressPicker.vue";
 
-describe("SfAddressPicker.vue", () => {
+describe("AwAddressPicker.vue", () => {
   it("renders a component", () => {
     const component = shallowMount(AwAddressPicker);
     expect(component.classes("sf-address-picker")).toBe(true);

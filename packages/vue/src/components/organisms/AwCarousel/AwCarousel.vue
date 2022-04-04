e191669510b6970efe5f1d6cf84b9b977e708fbb
ankurRaj-addweb <ankur.r@addwebsolution.in>
2022-04-04 16:49:36 +0530

    OrganismBugs

--- packages/vue/src/components/organisms/AwCarousel/AwCarousel.vue
+++ packages/vue/src/components/organisms/AwCarousel/AwCarousel.vue
@@ -21,7 +21,7 @@
       <div ref="glide" class="glide">
         <div class="glide__track" data-glide-el="track">
           <ul class="glide__slides sf-carousel__slides">
-            <slot />
+            <slot/>
           </ul>
         </div>
       </div>
